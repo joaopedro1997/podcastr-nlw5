854c2bb0c566820235923f0526ded227cf954e81
João Pedro Amaral Dias <joaopedrodiasamaral@gmail.com>
2021-05-12 20:49:37 -0300

    chore: add app.module.scss

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -1,6 +1,5 @@
-import React from "react";
 import styles from "./styles.module.scss";
 
 export function Player() {
-  return <div></div>;
+  return <div className={styles.playerContainer}></div>;
 }
